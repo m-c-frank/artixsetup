26cfbbd17358bd52d667d4b01ba35c2dbf60b1bf
Martin Christoph Frank <61345033+m-c-frank@users.noreply.github.com>
2023-09-07 19:59:20 +0200

    Update larbs.sh moved networking

--- larbs.sh
+++ larbs.sh
@@ -384,6 +384,8 @@ echo "kernel.dmesg_restrict = 0" > /etc/sysctl.d/dmesg.conf
 
 
 echo "m-c-frank"
+ln -s /etc/runit/sv/wpa_supplicant  /run/runit/service
+dhclient
 
 # Last message! Install complete!
 finalize
