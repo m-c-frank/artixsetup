b1f13291ae283e163e55a660b2bc4eb619e570a6
Martin Christoph Frank <61345033+m-c-frank@users.noreply.github.com>
2023-09-08 07:51:17 +0000

    using hopefully better way to setup the wifi connections

--- postboot.sh
+++ postboot.sh
@@ -1,7 +1,46 @@
-ln -s /etc/runit/sv/wpa_supplicant  /run/runit/service
-dhclient
+#!/bin/bash
 
-rm /var/wpa_supplicant/wlan0
-sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+# Exit on error
+set -e
+
+WLAN_INTERFACE="wlan0"
+
+# Copy wpa_supplicant configuration
+echo "Copying wpa_supplicant.conf to the appropriate location..."
+
+if [ -f ./wpa_supplicant.conf ]; then
+    cp ./wpa_supplicant.conf /etc/wpa_supplicant/
+else
+    echo "wpa_supplicant.conf not found in the current directory. Exiting."
+    exit 1
+fi
+
+echo "wpa_supplicant configuration done."
+
+# wpa_supplicant service
+echo "Setting up wpa_supplicant service..."
+
+if [ ! -L /run/runit/service/wpa_supplicant ]; then
+    ln -s /etc/runit/sv/wpa_supplicant /run/runit/service/
+fi
+
+echo "wpa_supplicant service setup done."
+
+# Configure wpa_supplicant run script to include dhclient
+echo "Configuring wpa_supplicant to run dhclient..."
+
+sed -i "/^exec/i\dhclient $WLAN_INTERFACE &" /etc/runit/sv/wpa_supplicant/run
+
+echo "dhclient setup done."
+
+# Restart wpa_supplicant
+echo "Restarting wpa_supplicant..."
+
+sv restart wpa_supplicant
+
+echo "wpa_supplicant restarted."
+
+# Final output
+echo "All done. Please check if you're connected and have an IP address using 'ip addr show $WLAN_INTERFACE'."
 
 pacman -S chromium
