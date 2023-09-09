538982a52e7bf849e05bdd14d3677751f7d13902
Martin Christoph Frank <61345033+m-c-frank@users.noreply.github.com>
2023-09-09 12:22:52 +0200

    Update chromiumsetup.sh

--- chromiumsetup.sh
+++ chromiumsetup.sh
@@ -9,7 +9,8 @@ if [ $? -eq 0 ]; then
     chromium "https://chrome.google.com/webstore/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb" \
             "https://chrome.google.com/webstore/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb" \
             "https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm" \
-            "https://chrome.google.com/webstore/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone"
+            "https://chrome.google.com/webstore/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone" \
+            "https://chrome.google.com/webstore/detail/video-speed-controller/nffaoalbilbmmfgbnbgppjihopabppdk/related"
 else
     echo "Error: Chromium installation failed."
 fi
