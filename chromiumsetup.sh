59015002d8cf196b635e2deb7b3bd1964d592d49
Martin Christoph Frank <61345033+m-c-frank@users.noreply.github.com>
2023-09-09 12:20:48 +0200

    Update chromiumsetup.sh

--- chromiumsetup.sh
+++ chromiumsetup.sh
@@ -6,7 +6,10 @@ sudo pacman -S chromium
 # Check if Chromium installed successfully
 if [ $? -eq 0 ]; then
     # Launch Chromium with specified pages
-    chromium "https://chrome.google.com/webstore/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb"
+    chromium "https://chrome.google.com/webstore/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb" \
+            "https://chrome.google.com/webstore/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb" \
+            "https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm" \
+            "https://chrome.google.com/webstore/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone"
 else
     echo "Error: Chromium installation failed."
 fi
